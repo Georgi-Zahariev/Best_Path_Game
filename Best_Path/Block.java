db370add26566cca06bfab73b39317bc53d8d433
Georgi-Zahariev <74987305+Georgi-Zahariev@users.noreply.github.com>
2023-11-07 21:08:37 -0500

    Abstract class

---
+++ Best_Path/Block.java
@@ -0,0 +1,10 @@
+/**
+ * Abstract class with a method
+ */
+public abstract class Block {
+    /**
+     * abstract class that will be overridden in the child classes
+     * @return int value
+     */
+    public abstract int getValue();
+}
